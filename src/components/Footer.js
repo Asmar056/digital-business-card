6e9e216668e43c6aa13a552794243bec7b03235a
Asmr056 <asmar.ismayilova.06@outlook.com>
2022-05-20 09:15:56 +0400

    digital-business-card is added

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Footer() {
+    return (
+        <footer>
+            <a href="https://github.com/Asmar056"><img src="images/github.png" className="footer-img"/></a>
+            <a href="https://www.facebook.com/asmarismayilova06/"><img src="images/facebook.png" className="footer-img"/></a>
+            <a href="https://www.instagram.com/girl_programmer_/?hl=en"><img src="images/instagram.png" className="footer-img"/></a>
+            <a href="https://twitter.com/home?lang=en"><img src="images/twitter.png" className="footer-img"/></a>
+
+        </footer>
+    )
+}
---
+++ src/components/Interest.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Interest(){
+    return(
+        <main className="interest">
+            <h5 className="interest-title title">Interests</h5>
+            <p className="interest-text text">
+                Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur.
+                Travel geek. Pop culture ninja. Coffee fanatic.
+            </p>
+        </main>
+    )
+}
